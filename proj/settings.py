51821bb9bab184c43b29d80faf83b5d944ee7da8
Youjung <handgonpo@naver.com>
2025-08-02 17:32:22 +0900

    Add STATIC_ROOT setting for collectstatic

--- proj/settings.py
+++ proj/settings.py
@@ -114,6 +114,9 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  # 추가!
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
